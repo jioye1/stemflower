9faf152fbf0c61b4efa60ea912e5faabbe6913fe
jioye1 <sejacksony@gmail.com>
2024-06-24 21:27:14 -0400

    commit4

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import {
     getFirestore, collection, getDocs, addDoc
 } from 'firebase/firestore';
 
-require('dotenv').config();
-
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
