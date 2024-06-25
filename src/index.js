d0d41ea57b4d3f90c41b18a035cf7bf2f71a3551
jioye1 <sejacksony@gmail.com>
2024-06-24 21:02:10 -0400

    environment variables

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB88vrwuNXpyIh79JeOQPxWqsIv2Edk9R4",
-    authDomain: "stemflowerapplication.firebaseapp.com",
-    projectId: "stemflowerapplication",
-    storageBucket: "stemflowerapplication.appspot.com",
-    messagingSenderId: "831239227290",
-    appId: "1:831239227290:web:1318e25f325a790ad99c23",
-    measurementId: "G-HCZ7ZHPX0H"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
