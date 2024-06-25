e1b426d6a435c9919d7494616f236c27313eeb47
jioye1 <sejacksony@gmail.com>
2024-06-24 21:13:34 -0400

    environment variables2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {
     getFirestore, collection, getDocs, addDoc
 } from 'firebase/firestore';
 
+require('dotenv').config();
+
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
