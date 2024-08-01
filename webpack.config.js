4577cf5c1b2cab78b6bcf785d32e8860b11a1f52
jioye1 <sejacksony@gmail.com>
2024-07-31 23:46:38 -0400

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // Your entry point
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader', // Use Babel to transpile your code if needed
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
+    mode: 'production'
 };
